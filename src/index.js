4ade26706d5b4eea562aa8f40bc9ffaaab0babc2
Hongbo He <hhbcarl@gmail.com>
2021-06-11 11:39:38 +0800

    Webpack: Assert Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import _ from 'lodash';
+import _ from 'lodash'
+import './style.css'
+import Icon from './icon.svg'
+import Notes from './data.csv'
 
-function component() {
-    const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    return element;
+function component () {
+    const element = document.createElement('div')
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    element.classList.add('hello')
+
+    const myIcon = new Image()
+    myIcon.src = Icon
+
+    element.appendChild(myIcon)
+
+    for (const n in Notes) {
+        const row = document.createElement('div')
+        row.innerHTML = _.join(Notes[n], ' ')
+        element.appendChild(row)
+    }
+
+    return element
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component())
