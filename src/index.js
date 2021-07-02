58094b0ea4fa0852288d21a1f2a79ad5956282c3
Hongbo He <hhbcarl@gmail.com>
2021-07-02 14:33:52 +0800

    Split to module

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import * as pg from './playground.js'
 import './style.css'
 
-class ShoppingList extends React.Component {
-    static get propTypes () {
-        return {
-            name: PropTypes.string
-        }
-    }
-
-    render () {
-        return (
-            <div className="shopping-list">
-                <h1>Shopping List for {this.props.name}</h1>
-                <ul>
-                    <li>Instagram</li>
-                    <li>WhatsApp</li>
-                    <li>Oculus</li>
-                </ul>
-            </div>
-        )
-    }
-}
-
 ReactDOM.render(
-    <ShoppingList name="Taylor" />,
+    // see: https://javascript.info/import-export#import
+    <pg.Clock />,
     document.getElementById('react-root')
 )
